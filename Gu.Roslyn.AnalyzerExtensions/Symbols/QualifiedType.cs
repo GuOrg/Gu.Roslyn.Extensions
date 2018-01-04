0f0daff34774499a321d5d84ec481c4baa608537
JohanLarsson <johan_gavle1@hotmail.com>
2018-01-04 20:27:05 +0100

    QualifiedType

---
+++ Gu.Roslyn.AnalyzerExtensions/Symbols/QualifiedType.cs
@@ -0,0 +1,42 @@
+namespace Gu.Roslyn.AnalyzerExtensions
+{
+    using Microsoft.CodeAnalysis;
+
+    [System.Diagnostics.DebuggerDisplay("FullName: {FullName}")]
+    public class QualifiedType
+    {
+        public readonly string FullName;
+        public readonly NamespaceParts Namespace;
+        public readonly string Type;
+
+        public QualifiedType(string fullName)
+            : this(fullName, NamespaceParts.Create(fullName), fullName.Substring(fullName.LastIndexOf('.') + 1))
+        {
+        }
+
+        private QualifiedType(string fullName, NamespaceParts @namespace, string type)
+        {
+            this.FullName = fullName;
+            this.Namespace = @namespace;
+            this.Type = type;
+        }
+
+        public static bool operator ==(ITypeSymbol left, QualifiedType right)
+        {
+            if (left == null && right == null)
+            {
+                return true;
+            }
+
+            if (left == null || right == null)
+            {
+                return false;
+            }
+
+            return left.MetadataName == right.Type &&
+                   left.ContainingNamespace == right.Namespace;
+        }
+
+        public static bool operator !=(ITypeSymbol left, QualifiedType right) => !(left == right);
+    }
+}
