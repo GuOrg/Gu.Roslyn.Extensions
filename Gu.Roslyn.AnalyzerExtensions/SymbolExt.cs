29c15672f06af22de7f057333c15e805d03ce211
JohanLarsson <johan_gavle1@hotmail.com>
2018-03-22 10:59:16 +0100

    TrySingleDeclaration

--- Gu.Roslyn.AnalyzerExtensions/SymbolExt.cs
+++ Gu.Roslyn.AnalyzerExtensions/SymbolExt.cs
@@ -13,7 +13,67 @@ namespace Gu.Roslyn.AnalyzerExtensions
             return symbol is T1 || symbol is T2;
         }
 
-        public static bool TryGetSingleDeclaration<T>(this ISymbol symbol, CancellationToken cancellationToken, out T declaration)
+        internal static bool TrySingleDeclaration(this IFieldSymbol field, CancellationToken cancellationToken, out FieldDeclarationSyntax declaration)
+        {
+            declaration = null;
+            if (field != null &&
+                field.DeclaringSyntaxReferences.TrySingle(out var reference))
+            {
+                declaration = reference.GetSyntax(cancellationToken).FirstAncestorOrSelf<FieldDeclarationSyntax>();
+            }
+
+            return declaration != null;
+        }
+
+        internal static bool TrySingleDeclaration(this IPropertySymbol property, CancellationToken cancellationToken, out PropertyDeclarationSyntax declaration)
+        {
+            declaration = null;
+            if (property != null &&
+                property.DeclaringSyntaxReferences.TrySingle(out var reference))
+            {
+                declaration = reference.GetSyntax(cancellationToken) as PropertyDeclarationSyntax;
+            }
+
+            return declaration != null;
+        }
+
+        internal static bool TrySingleDeclaration(this IMethodSymbol method, CancellationToken cancellationToken, out MethodDeclarationSyntax declaration)
+        {
+            declaration = null;
+            if (method != null &&
+                method.DeclaringSyntaxReferences.TrySingle(out var reference))
+            {
+                declaration = reference.GetSyntax(cancellationToken) as MethodDeclarationSyntax;
+            }
+
+            return declaration != null;
+        }
+
+        internal static bool TrySingleDeclaration(this IParameterSymbol parameter, CancellationToken cancellationToken, out ParameterSyntax declaration)
+        {
+            declaration = null;
+            if (parameter != null &&
+                parameter.DeclaringSyntaxReferences.TrySingle(out var reference))
+            {
+                declaration = reference.GetSyntax(cancellationToken) as ParameterSyntax;
+            }
+
+            return declaration != null;
+        }
+
+        internal static bool TrySingleDeclaration(this ILocalSymbol local, CancellationToken cancellationToken, out VariableDeclarationSyntax declaration)
+        {
+            declaration = null;
+            if (local != null &&
+                local.DeclaringSyntaxReferences.TrySingle(out var reference))
+            {
+                declaration = reference.GetSyntax(cancellationToken).FirstAncestorOrSelf<VariableDeclarationSyntax>();
+            }
+
+            return declaration != null;
+        }
+
+        internal static bool TrySingleDeclaration<T>(this ISymbol symbol, CancellationToken cancellationToken, out T declaration)
             where T : SyntaxNode
         {
             declaration = null;
