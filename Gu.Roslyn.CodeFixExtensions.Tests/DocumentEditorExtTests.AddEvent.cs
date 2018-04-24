339944fe3ef0a5010891a78a63ae485c022fb862
JohanLarsson <johan_gavle1@hotmail.com>
2018-04-24 09:20:56 +0200

    Test AddEvent

---
+++ Gu.Roslyn.CodeFixExtensions.Tests/DocumentEditorExtTests.AddEvent.cs
@@ -0,0 +1,144 @@
+namespace Gu.Roslyn.CodeFixExtensions.Tests
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Gu.Roslyn.Asserts;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using Microsoft.CodeAnalysis.Editing;
+    using NUnit.Framework;
+
+    public partial class DocumentEditorExtTests
+    {
+        public class AddEvent
+        {
+            [Test]
+            public async Task AddEventFieldDeclarationSyntax()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public abstract class Foo
+    {
+        public int Filed1 = 1;
+        private int filed1;
+
+        public Foo()
+        {
+        }
+
+        private Foo(int i)
+        {
+        }
+
+        public int Prop1 { get; set; }
+
+        public void Bar1()
+        {
+        }
+    }
+}";
+                var sln = CodeFactory.CreateSolution(testCode);
+                var editor = await DocumentEditor.CreateAsync(sln.Projects.First().Documents.First()).ConfigureAwait(false);
+                var eventDeclaration = (EventFieldDeclarationSyntax)editor.Generator.EventDeclaration("SomeEvent", SyntaxFactory.ParseTypeName("System.EventHandler"), Accessibility.Public);
+                var containingType = SyntaxNodeExt.FindClassDeclaration(editor.OriginalRoot.SyntaxTree, "Foo");
+                editor.AddEvent(containingType, eventDeclaration);
+                var expected = @"
+namespace RoslynSandbox
+{
+    public abstract class Foo
+    {
+        public int Filed1 = 1;
+        private int filed1;
+
+        public Foo()
+        {
+        }
+
+        private Foo(int i)
+        {
+        }
+
+        public event System.EventHandler SomeEvent;
+
+        public int Prop1 { get; set; }
+
+        public void Bar1()
+        {
+        }
+    }
+}";
+                CodeAssert.AreEqual(expected, editor.GetChangedDocument());
+            }
+
+            [Test]
+            public async Task AddEventDeclarationSyntax()
+            {
+                var testCode = @"
+namespace RoslynSandbox
+{
+    public abstract class Foo
+    {
+        public int Filed1 = 1;
+        private int filed1;
+
+        public Foo()
+        {
+        }
+
+        private Foo(int i)
+        {
+        }
+
+        public int Prop1 { get; set; }
+
+        public void Bar1()
+        {
+        }
+    }
+}";
+                var sln = CodeFactory.CreateSolution(testCode);
+                var editor = await DocumentEditor.CreateAsync(sln.Projects.First().Documents.First()).ConfigureAwait(false);
+                var eventDeclaration = (EventDeclarationSyntax)editor.Generator.CustomEventDeclaration("SomeEvent", SyntaxFactory.ParseTypeName("System.EventHandler"), Accessibility.Public);
+                var containingType = SyntaxNodeExt.FindClassDeclaration(editor.OriginalRoot.SyntaxTree, "Foo");
+                editor.AddEvent(containingType, eventDeclaration);
+                var expected = @"
+namespace RoslynSandbox
+{
+    public abstract class Foo
+    {
+        public int Filed1 = 1;
+        private int filed1;
+
+        public Foo()
+        {
+        }
+
+        private Foo(int i)
+        {
+        }
+
+        public event System.EventHandler SomeEvent
+        {
+            add
+            {
+            }
+
+            remove
+            {
+            }
+        }
+
+        public int Prop1 { get; set; }
+
+        public void Bar1()
+        {
+        }
+    }
+}";
+                CodeAssert.AreEqual(expected, editor.GetChangedDocument());
+            }
+        }
+    }
+}
