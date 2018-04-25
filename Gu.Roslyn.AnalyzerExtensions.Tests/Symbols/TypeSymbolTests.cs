a18c126d53dcd4f0cbd4fa36a2a360af47b622ab
Johan Larsson <johan_gavle1@hotmail.com>
2018-04-25 07:49:11 +0200

    Failing tests.

--- Gu.Roslyn.AnalyzerExtensions.Tests/Symbols/TypeSymbolTests.cs
+++ Gu.Roslyn.AnalyzerExtensions.Tests/Symbols/TypeSymbolTests.cs
@@ -1,20 +1,19 @@
 namespace Gu.Roslyn.AnalyzerExtensions.Tests.Symbols
 {
+    using System.Threading;
     using Gu.Roslyn.Asserts;
     using Microsoft.CodeAnalysis.CSharp;
     using NUnit.Framework;
 
-    public class TypeSymbolTests
+    public partial class TypeSymbolTests
     {
-        public class Is
+        [TestCase("int value1, int value2")]
+        [TestCase("int value1, System.IComparable value2")]
+        [TestCase("int value1, System.IComparable<int> value2")]
+        [TestCase("int value1, object value2")]
+        public void WhenTrue(string parameters)
         {
-            [TestCase("int value1, int value2")]
-            [TestCase("int value1, System.IComparable value2")]
-            [TestCase("int value1, System.IComparable<int> value2")]
-            [TestCase("int value1, object value2")]
-            public void WhenTrue(string parameters)
-            {
-                var code = @"
+            var code = @"
 namespace RoslynSandbox
 {
     public class Foo
@@ -24,38 +23,16 @@ namespace RoslynSandbox
         }
     }
 }";
-                code = code.AssertReplace("int value1, int value2", parameters);
-                var syntaxTree = CSharpSyntaxTree.ParseText(code);
-                var compilation = CSharpCompilation.Create("test", new[] { syntaxTree }, MetadataReferences.FromAttributes());
-                var semanticModel = compilation.GetSemanticModel(syntaxTree);
-                var ctor = semanticModel.GetDeclaredSymbol(syntaxTree.FindConstructorDeclaration("Foo"));
-                var type1 = ctor.Parameters[0].Type;
-                var type2 = ctor.Parameters[1].Type;
-                Assert.AreEqual(true, type1.Is(type2));
-            }
-
-            [Test]
-            public void Inheritance()
-            {
-                var code = @"
-namespace RoslynSandbox
-{
-    class A
-    {
-    }
-
-    class B : A
-    {
-    }
-}";
-                var syntaxTree = CSharpSyntaxTree.ParseText(code);
-                var compilation = CSharpCompilation.Create("test", new[] { syntaxTree }, MetadataReferences.FromAttributes());
-                var semanticModel = compilation.GetSemanticModel(syntaxTree);
-                var a = semanticModel.GetDeclaredSymbol(syntaxTree.FindClassDeclaration("A"));
-                var b = semanticModel.GetDeclaredSymbol(syntaxTree.FindClassDeclaration("B"));
-                Assert.AreEqual(false, a.Is(b));
-                Assert.AreEqual(true, b.Is(a));
-            }
+            code = code.AssertReplace("int value1, int value2", parameters);
+            var syntaxTree = CSharpSyntaxTree.ParseText(code);
+            var compilation = CSharpCompilation.Create("test", new[] { syntaxTree }, MetadataReferences.FromAttributes());
+            var semanticModel = compilation.GetSemanticModel(syntaxTree);
+            var constructorDeclaration = syntaxTree.FindConstructorDeclaration("Foo");
+            var ctor = semanticModel.GetDeclaredSymbol(constructorDeclaration);
+            var type1 = ctor.Parameters[0].Type;
+            var type2 = ctor.Parameters[1].Type;
+            var typeSyntax = constructorDeclaration.ParameterList.Parameters[1].Type;
+            Assert.AreEqual(true, type1.IsRepresentationPreservingConversion(typeSyntax, semanticModel, CancellationToken.None));
         }
     }
 }
