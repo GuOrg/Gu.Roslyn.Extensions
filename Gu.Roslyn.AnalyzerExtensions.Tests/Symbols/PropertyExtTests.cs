32bb77a381a4a4f51c97e6c70bf3c18f90e64404
JohanLarsson <johan_gavle1@hotmail.com>
2018-05-08 08:53:50 +0200

    PropertyExt

---
+++ Gu.Roslyn.AnalyzerExtensions.Tests/Symbols/PropertyExtTests.cs
@@ -0,0 +1,57 @@
+namespace Gu.Roslyn.AnalyzerExtensions.Tests.Symbols
+{
+    using Gu.Roslyn.Asserts;
+    using Microsoft.CodeAnalysis.CSharp;
+    using NUnit.Framework;
+
+    public class PropertyExtTests
+    {
+        [TestCase("GetOnly", true)]
+        [TestCase("AutoGetSet", true)]
+        [TestCase("ExpressionBody", false)]
+        public void IsAutoProperty(string name, bool expected)
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(
+                @"
+namespace RoslynSandbox
+{
+    public class Foo
+    {
+        public int GetOnly { get; }
+
+        public int AutoGetSet { get; set; }
+
+        public int ExpressionBody => GetOnly;
+    }
+}");
+            var compilation = CSharpCompilation.Create("test", new[] { syntaxTree }, MetadataReferences.FromAttributes());
+            var semanticModel = compilation.GetSemanticModel(syntaxTree);
+            var property = semanticModel.GetDeclaredSymbol(syntaxTree.FindPropertyDeclaration(name));
+            Assert.AreEqual(expected, property.IsAutoProperty());
+        }
+
+        [TestCase("GetOnly", true)]
+        [TestCase("AutoGetSet", false)]
+        [TestCase("ExpressionBody", false)]
+        public void IsGetOnly(string name, bool expected)
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(
+                @"
+namespace RoslynSandbox
+{
+    public class Foo
+    {
+        public int GetOnly { get; }
+
+        public int AutoGetSet { get; set; }
+
+        public int ExpressionBody => GetOnly;
+    }
+}");
+            var compilation = CSharpCompilation.Create("test", new[] { syntaxTree }, MetadataReferences.FromAttributes());
+            var semanticModel = compilation.GetSemanticModel(syntaxTree);
+            var property = semanticModel.GetDeclaredSymbol(syntaxTree.FindPropertyDeclaration(name));
+            Assert.AreEqual(expected, property.IsGetOnly());
+        }
+    }
+}
---
+++ Gu.Roslyn.AnalyzerExtensions/Symbols/PropertyExt.cs
@@ -0,0 +1,94 @@
+namespace Gu.Roslyn.AnalyzerExtensions
+{
+    using System.Threading;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+    /// <summary>
+    /// Helpers for working with <see cref="IPropertySymbol"/>
+    /// </summary>
+    public static class PropertyExt
+    {
+        /// <summary>
+        /// Get the declaration of the GetMethod if any.
+        /// Can be the expression body or the get accessor.
+        /// </summary>
+        /// <param name="property">The <see cref="IPropertySymbol"/></param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/></param>
+        /// <param name="declaration">The declaration</param>
+        /// <returns>True if a declaration was found.</returns>
+        public static bool TryGetGetMethodDeclaration(this IPropertySymbol property, CancellationToken cancellationToken, out SyntaxNode declaration)
+        {
+            declaration = null;
+            return property.GetMethod is IMethodSymbol getMethod &&
+                   getMethod.TrySingleDeclaration(cancellationToken, out declaration);
+        }
+
+        /// <summary>
+        /// Get the get accessor if any.
+        /// </summary>
+        /// <param name="property">The <see cref="IPropertySymbol"/></param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/></param>
+        /// <param name="getter">The declaration</param>
+        /// <returns>True if a declaration was found.</returns>
+        public static bool TryGetGetter(this IPropertySymbol property, CancellationToken cancellationToken, out AccessorDeclarationSyntax getter)
+        {
+            getter = null;
+            return property.GetMethod != null &&
+                   property.TrySingleDeclaration(cancellationToken, out var declaration) &&
+                   declaration.TryGetGetter(out getter);
+        }
+
+        /// <summary>
+        /// Get the set accessor if any.
+        /// </summary>
+        /// <param name="property">The <see cref="IPropertySymbol"/></param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/></param>
+        /// <param name="setter">The declaration</param>
+        /// <returns>True if a declaration was found.</returns>
+        public static bool TryGetSetter(this IPropertySymbol property, CancellationToken cancellationToken, out AccessorDeclarationSyntax setter)
+        {
+            setter = null;
+            return property.SetMethod != null &&
+                   property.TrySingleDeclaration(cancellationToken, out var declaration) &&
+                   declaration.TryGetSetter(out setter);
+        }
+
+        /// <summary>
+        /// Check if the property is an auto property with get only.
+        /// public int Value { get; }
+        /// </summary>
+        /// <param name="property">The <see cref="IPropertySymbol"/></param>
+        /// <returns>True if the property is an auto property with get only.</returns>
+        public static bool IsGetOnly(this IPropertySymbol property)
+        {
+            return property.SetMethod == null &&
+                   property.IsAutoProperty();
+        }
+
+        /// <summary>
+        /// Check if the property is an auto property.
+        /// public int Value { get; private set; }
+        /// </summary>
+        /// <param name="property">The <see cref="IPropertySymbol"/></param>
+        /// <returns>True if the property is an auto property.</returns>
+        public static bool IsAutoProperty(this IPropertySymbol property)
+        {
+            if (property == null)
+            {
+                return false;
+            }
+
+            foreach (var member in property.ContainingType.GetMembers())
+            {
+                if (member is IFieldSymbol field &&
+                    field.AssociatedSymbol.Equals(property))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
