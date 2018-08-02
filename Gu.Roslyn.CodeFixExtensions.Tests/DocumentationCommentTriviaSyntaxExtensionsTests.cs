05e9539f25d9dc7f7a66e6eb4968db49932bbea2
JohanLarsson <johan_gavle1@hotmail.com>
2018-08-02 15:19:18 +0200

    Assert AST

--- Gu.Roslyn.CodeFixExtensions.Tests/DocumentationCommentTriviaSyntaxExtensionsTests.cs
+++ Gu.Roslyn.CodeFixExtensions.Tests/DocumentationCommentTriviaSyntaxExtensionsTests.cs
@@ -1,15 +1,86 @@
 namespace Gu.Roslyn.CodeFixExtensions.Tests
 {
-    using System.Text;
     using Gu.Roslyn.AnalyzerExtensions;
     using Gu.Roslyn.Asserts;
-    using Microsoft.CodeAnalysis;
     using Microsoft.CodeAnalysis.CSharp;
     using Microsoft.CodeAnalysis.CSharp.Syntax;
     using NUnit.Framework;
 
     public class DocumentationCommentTriviaSyntaxExtensionsTests
     {
+        [Test]
+        public void WithSingleLineSummary()
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(@"
+namespace RoslynSandbox
+{
+    public class Foo
+    {
+        /// <remarks></remarks>
+        public void Bar()
+        {
+        }
+    }
+}");
+            var method = syntaxTree.FindMethodDeclaration("Bar");
+            Assert.AreEqual(true, method.TryGetDocumentationComment(out var comment));
+            var updated = comment.WithSummary("New text.");
+            Assert.AreEqual(true, updated.TryGetSummary(out var summary));
+            Assert.AreEqual("<summary> New text. </summary>", summary.ToFullString());
+
+            var expected = GetExpected(@"
+namespace RoslynSandbox
+{
+    public class Foo
+    {
+        /// <summary> New text. </summary>
+        /// <remarks></remarks>
+        public void Bar()
+        {
+        }
+    }
+}");
+            AssertAst(expected, updated);
+        }
+
+        [Test]
+        public void WithMultiLineSummary()
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(@"
+namespace RoslynSandbox
+{
+    public class Foo
+    {
+        /// <remarks></remarks>
+        public void Bar()
+        {
+        }
+    }
+}");
+            var method = syntaxTree.FindMethodDeclaration("Bar");
+            Assert.AreEqual(true, method.TryGetDocumentationComment(out var comment));
+            var updated = comment.WithSummary("New\r\ntext.");
+            Assert.AreEqual(true, updated.TryGetSummary(out var summary));
+            Assert.AreEqual("<summary> New\r\n text.\r\n </summary>", summary.ToFullString());
+
+            var expected = GetExpected(@"
+namespace RoslynSandbox
+{
+    public class Foo
+    {
+        /// <summary>
+        /// New
+        /// text.
+        /// </summary>
+        /// <remarks></remarks>
+        public void Bar()
+        {
+        }
+    }
+}");
+            AssertAst(expected, updated);
+        }
+
         [Test]
         public void WithSingleLineSummaryReplaceOldSingleLine()
         {
@@ -86,38 +157,17 @@ namespace RoslynSandbox
 
         private static void AssertAst(DocumentationCommentTriviaSyntax expected, DocumentationCommentTriviaSyntax actual)
         {
-            var expectedAst = DumpAst(expected);
-            var actualAst = DumpAst(actual);
+            var expectedAst = Dump.Ast(expected);
+            var actualAst = Dump.Ast(actual);
             CodeAssert.AreEqual(expectedAst, actualAst);
         }
 
         private static DocumentationCommentTriviaSyntax GetExpected(string code)
         {
             var syntaxTree = CSharpSyntaxTree.ParseText(code);
-            var method = syntaxTree.FindMethodDeclaration("Id");
+            var method = syntaxTree.FindMethodDeclaration("(");
             Assert.AreEqual(true, method.TryGetDocumentationComment(out var comment));
             return comment;
-        }
-
-        private static string DumpAst(SyntaxNode node)
-        {
-            var builder = new StringBuilder();
-            builder.AppendLine($"{node.Kind()} {node.ToString().Replace("\r", "\\r").Replace("\n", "\\n")}");
-            foreach (var child in node.DescendantNodesAndTokens())
-            {
-                DumpAst(child, builder);
-            }
-
-            return builder.ToString();
-        }
-
-        private static void DumpAst(SyntaxNodeOrToken nodeOrToken, StringBuilder builder)
-        {
-            builder.AppendLine($"{nodeOrToken.Kind()} {nodeOrToken.ToString().Replace("\r", "\\r").Replace("\n", "\\n")}");
-            foreach (var child in nodeOrToken.ChildNodesAndTokens())
-            {
-                DumpAst(child, builder);
-            }
         }
     }
 }
--- Gu.Roslyn.CodeFixExtensions/DocumentationCommentTriviaSyntaxExtensions.cs
+++ Gu.Roslyn.CodeFixExtensions/DocumentationCommentTriviaSyntaxExtensions.cs
@@ -3,6 +3,7 @@ namespace Gu.Roslyn.CodeFixExtensions
     using System;
     using Gu.Roslyn.AnalyzerExtensions;
     using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
     using Microsoft.CodeAnalysis.CSharp.Syntax;
 
     /// <summary>
@@ -24,7 +25,23 @@ namespace Gu.Roslyn.CodeFixExtensions
                 return comment.ReplaceNode(old, summary);
             }
 
-            throw new NotImplementedException();
+            return comment.WithContent(comment.Content.InsertRange(1, new XmlNodeSyntax[] { summary, comment.NewLine() }));
+        }
+
+        private static XmlTextSyntax NewLine(this DocumentationCommentTriviaSyntax comment)
+        {
+            var leadingWhiteSpace = comment.ParentTrivia.Token.LeadingTrivia.TryFirst(x => x.IsKind(SyntaxKind.WhitespaceTrivia), out var whitespaceTrivia)
+                    ? whitespaceTrivia.ToString()
+                    : "        ";
+            return SyntaxFactory.XmlText(
+                                    SyntaxFactory.Token(SyntaxTriviaList.Empty, SyntaxKind.XmlTextLiteralNewLineToken, Environment.NewLine, Environment.NewLine, SyntaxTriviaList.Empty),
+                                    SyntaxFactory.Token(
+                                        leading: SyntaxFactory.TriviaList(SyntaxFactory.SyntaxTrivia(SyntaxKind.DocumentationCommentExteriorTrivia, $"{leadingWhiteSpace}///")),
+                                        kind: SyntaxKind.XmlTextLiteralToken,
+                                        text: " ",
+                                        valueText: " ",
+                                        trailing: SyntaxTriviaList.Empty))
+                                .WithLeadingTrivia(SyntaxTriviaList.Empty);
         }
     }
 }
