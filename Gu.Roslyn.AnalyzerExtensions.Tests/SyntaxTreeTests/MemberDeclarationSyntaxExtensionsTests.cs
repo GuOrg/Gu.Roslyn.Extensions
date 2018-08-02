4ed151090e61d3b91d8c349d1f91c5a78f8dcedc
JohanLarsson <johan_gavle1@hotmail.com>
2018-08-02 09:08:26 +0200

    MemberDeclarationSyntaxExtensions.TryGetDocumentationComment. #11

---
+++ Gu.Roslyn.AnalyzerExtensions.Tests/SyntaxTreeTests/MemberDeclarationSyntaxExtensionsTests.cs
@@ -0,0 +1,86 @@
+namespace Gu.Roslyn.AnalyzerExtensions.Tests.SyntaxTreeTests
+{
+    using Gu.Roslyn.Asserts;
+    using Microsoft.CodeAnalysis.CSharp;
+    using NUnit.Framework;
+
+    public class MemberDeclarationSyntaxExtensionsTests
+    {
+        [Test]
+        public void Class()
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(@"
+namespace RoslynSandbox
+{
+    /// <summary>
+    /// The Foo
+    /// </summary>
+    public class Foo
+    {
+    }
+}");
+            var property = syntaxTree.FindClassDeclaration("Foo");
+            Assert.AreEqual(true, property.TryGetDocumentationComment(out var result));
+            Assert.AreEqual("/// <summary>\r\n    /// The Foo\r\n    /// </summary>\r\n", result.ToFullString());
+        }
+
+        [Test]
+        public void Constructor()
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(@"
+namespace RoslynSandbox
+{
+    public class Foo
+    {
+        /// <summary> Initializes a new instance of the <see cref=""Foo""/> class. </summary>
+        public Foo()
+        {
+        }
+    }
+}");
+            var property = syntaxTree.FindConstructorDeclaration("Foo");
+            Assert.AreEqual(true, property.TryGetDocumentationComment(out var result));
+            Assert.AreEqual("/// <summary> Initializes a new instance of the <see cref=\"Foo\"/> class. </summary>\r\n", result.ToFullString());
+        }
+
+        [Test]
+        public void Property()
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(@"
+namespace RoslynSandbox
+{
+    public class Foo
+    {
+        /// <summary>
+        /// Gets or sets the value
+        /// </summary>
+        public int Value { get; set; }
+    }
+}");
+            var property = syntaxTree.FindPropertyDeclaration("Value");
+            Assert.AreEqual(true, property.TryGetDocumentationComment(out var result));
+            Assert.AreEqual("/// <summary>\r\n        /// Gets or sets the value\r\n        /// </summary>\r\n", result.ToFullString());
+        }
+
+        [Test]
+        public void Method()
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(@"
+namespace RoslynSandbox
+{
+    public class Foo
+    {
+        /// <summary>
+        /// The identity function.
+        /// </summary>
+        /// <param name=""i"">The value to return.</param>
+        /// <returns><paramref name=""i""/></returns>
+        public int Id(int i) => i;
+    }
+}");
+            var property = syntaxTree.FindMethodDeclaration("Id");
+            Assert.AreEqual(true, property.TryGetDocumentationComment(out var result));
+            Assert.AreEqual("/// <summary>\r\n        /// The identity function.\r\n        /// </summary>\r\n        /// <param name=\"i\">The value to return.</param>\r\n        /// <returns><paramref name=\"i\"/></returns>\r\n", result.ToFullString());
+        }
+    }
+}
---
+++ Gu.Roslyn.AnalyzerExtensions/SyntaxTree/MemberDeclarationSyntaxExtensions.cs
@@ -0,0 +1,29 @@
+namespace Gu.Roslyn.AnalyzerExtensions
+{
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+    public static class MemberDeclarationSyntaxExtensions
+    {
+        /// <summary>
+        /// Get the <see cref="DocumentationCommentTriviaSyntax"/> for the member if it exists.
+        /// </summary>
+        /// <param name="member">The <see cref="MemberDeclarationSyntax"/></param>
+        /// <param name="comment">The returned <see cref="DocumentationCommentTriviaSyntax"/>.</param>
+        /// <returns>True if a single <see cref="DocumentationCommentTriviaSyntax"/> was found.</returns>
+        public static bool TryGetDocumentationComment(this MemberDeclarationSyntax member, out DocumentationCommentTriviaSyntax comment)
+        {
+            if (member.HasLeadingTrivia &&
+                member.GetLeadingTrivia() is SyntaxTriviaList triviaList &&
+                triviaList.TrySingle(x => x.HasStructure, out var commentTrivia) &&
+                commentTrivia.GetStructure() is DocumentationCommentTriviaSyntax triviaSyntax)
+            {
+                comment = triviaSyntax;
+                return true;
+            }
+
+            comment = null;
+            return false;
+        }
+    }
+}
