b3eae10d667b26eaf688f36224b18933d3be0f60
JohanLarsson <johan_gavle1@hotmail.com>
2018-08-02 16:54:37 +0200

    Docs for WithSummary

--- Gu.Roslyn.CodeFixExtensions/DocumentationCommentTriviaSyntaxExtensions.cs
+++ Gu.Roslyn.CodeFixExtensions/DocumentationCommentTriviaSyntaxExtensions.cs
@@ -11,13 +11,25 @@ namespace Gu.Roslyn.CodeFixExtensions
     /// </summary>
     public static class DocumentationCommentTriviaSyntaxExtensions
     {
+        /// <summary>
+        /// Add a summary element to <paramref name="comment"/>
+        /// Replace if a summary element exists.
+        /// </summary>
+        /// <param name="comment">The <see cref="DocumentationCommentTriviaSyntax"/></param>
+        /// <param name="text"> The text to add inside the summary element.</param>
+        /// <returns><paramref name="comment"/> with summary.</returns>
         public static DocumentationCommentTriviaSyntax WithSummary(this DocumentationCommentTriviaSyntax comment, string text)
         {
-            return comment.WithSummary(text.IndexOf('\n') < 0
-                ? Parse.XmlElementSyntax($"<summary> {text} </summary>")
-                : Parse.XmlElementSyntax($"<summary> \r\n {text} \r\n </summary>"));
+            return comment.WithSummary(Parse.XmlElementSyntax($"<summary> {text} </summary>"));
         }
 
+        /// <summary>
+        /// Add <paramref name="summary"/> element to <paramref name="comment"/>
+        /// Replace if a summary element exists.
+        /// </summary>
+        /// <param name="comment">The <see cref="DocumentationCommentTriviaSyntax"/></param>
+        /// <param name="summary"> The <see cref="XmlElementSyntax"/>.</param>
+        /// <returns><paramref name="comment"/> with <paramref name="summary"/>.</returns>
         public static DocumentationCommentTriviaSyntax WithSummary(this DocumentationCommentTriviaSyntax comment, XmlElementSyntax summary)
         {
             if (comment.TryGetSummary(out var old))
