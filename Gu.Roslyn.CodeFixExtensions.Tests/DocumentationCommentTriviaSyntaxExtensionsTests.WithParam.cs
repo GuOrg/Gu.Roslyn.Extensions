5da79d36a14ccb62cbf2415614cd78483b6f86a7
Johan Larsson <johan_gavle1@hotmail.com>
2018-08-03 18:04:59 +0200

    More failing tests.

--- Gu.Roslyn.CodeFixExtensions.Tests/DocumentationCommentTriviaSyntaxExtensionsTests.WithParam.cs
+++ Gu.Roslyn.CodeFixExtensions.Tests/DocumentationCommentTriviaSyntaxExtensionsTests.WithParam.cs
@@ -47,16 +47,14 @@ namespace RoslynSandbox
             }
 
             [Test]
-            public void WhenSummaryOnly()
+            public void WhenSummaryOnlySingleLine()
             {
                 var syntaxTree = CSharpSyntaxTree.ParseText(@"
 namespace RoslynSandbox
 {
     public class Foo
     {
-        /// <summary>
-        /// The identity function.
-        /// </summary>
+        /// <summary>The identity function.</summary>
         public int Id(int i) => i;
     }
 }");
@@ -65,9 +63,7 @@ namespace RoslynSandbox
 {
     public class Foo
     {
-        /// <summary>
-        /// The identity function.
-        /// </summary>
+        /// <summary>The identity function.</summary>
         /// <param name=""i"">The <see cref=""int""/> to return.</param>
         public int Id(int i) => i;
     }
@@ -82,7 +78,43 @@ namespace RoslynSandbox
             }
 
             [Test]
-            public void WhenReturnsOnly()
+            public void WhenSummaryOnlyMultiLine()
+            {
+                var syntaxTree = CSharpSyntaxTree.ParseText(@"
+namespace RoslynSandbox
+{
+    public class Foo
+    {
+        /// <summary>
+        /// The identity function.
+        /// </summary>
+        public int Id(int i) => i;
+    }
+}");
+                var expected = GetExpected(@"
+namespace RoslynSandbox
+{
+    public class Foo
+    {
+        /// <summary>
+        /// The identity function.
+        /// </summary>
+        /// <param name=""i"">The <see cref=""int""/> to return.</param>
+        public int Id(int i) => i;
+    }
+}");
+                var method = syntaxTree.FindMethodDeclaration("Id");
+                Assert.AreEqual(true, method.TryGetDocumentationComment(out var comment));
+                var updated = comment.WithParamText("i", "The <see cref=\"int\"/> to return.");
+                var text = updated.ToFullString();
+                AnalyzerAssert.Ast(expected, updated);
+
+                updated = comment.WithParam(Parse.XmlElementSyntax("<param name=\"i\">The <see cref=\"int\"/> to return.</param>", "        "));
+                AnalyzerAssert.Ast(expected, updated);
+            }
+
+            [Test]
+            public void WhenReturnsOnlySingleLine()
             {
                 var syntaxTree = CSharpSyntaxTree.ParseText(@"
 namespace RoslynSandbox
@@ -109,6 +141,111 @@ namespace RoslynSandbox
                 AnalyzerAssert.Ast(expected, updated);
 
                 updated = comment.WithParam(Parse.XmlElementSyntax("<param name=\"i\">The <see cref=\"int\"/> to return.</param>", "        "));
+                AnalyzerAssert.Ast(expected, updated);
+            }
+
+            [Test]
+            public void WhenReturnsOnlyMultiLine()
+            {
+                var syntaxTree = CSharpSyntaxTree.ParseText(@"
+namespace RoslynSandbox
+{
+    public class Foo
+    {
+        /// <returns>
+        /// The value passed in.
+        /// </returns>
+        public int Id(int i) => i;
+    }
+}");
+                var expected = GetExpected(@"
+namespace RoslynSandbox
+{
+    public class Foo
+    {
+        /// <param name=""i"">The <see cref=""int""/> to return.</param>
+        /// <returns>
+        /// The value passed in.
+        /// </returns>
+        public int Id(int i) => i;
+    }
+}");
+                var method = syntaxTree.FindMethodDeclaration("Id");
+                Assert.AreEqual(true, method.TryGetDocumentationComment(out var comment));
+                var updated = comment.WithParamText("i", "The <see cref=\"int\"/> to return.");
+                AnalyzerAssert.Ast(expected, updated);
+
+                updated = comment.WithParam(Parse.XmlElementSyntax("<param name=\"i\">The <see cref=\"int\"/> to return.</param>", "        "));
+                AnalyzerAssert.Ast(expected, updated);
+            }
+
+            [Test]
+            public void WhenParamExistsInserAfter()
+            {
+                var syntaxTree = CSharpSyntaxTree.ParseText(@"
+namespace RoslynSandbox
+{
+    public class Foo
+    {
+        /// <summary>Sum two numbers.</summary>
+        /// <param name=""x"">The first <see cref=""int""/> term.</param>
+        /// <returns>The sum.</returns>
+        public int Add(int x, int y) => x + y;
+    }
+}");
+                var expected = GetExpected(@"
+namespace RoslynSandbox
+{
+    public class Foo
+    {
+        /// <summary>Sum two numbers.</summary>
+        /// <param name=""x"">The first <see cref=""int""/> term.</param>
+        /// <param name=""y"">The other <see cref=""int""/> term.</param>
+        /// <returns>The sum.</returns>
+        public int Add(int x, int y) => x + y;
+    }
+}");
+                var method = syntaxTree.FindMethodDeclaration("Add");
+                Assert.AreEqual(true, method.TryGetDocumentationComment(out var comment));
+                var updated = comment.WithParamText("y", "The other <see cref=\"int\"/> term.");
+                AnalyzerAssert.Ast(expected, updated);
+
+                updated = comment.WithParam(Parse.XmlElementSyntax("<param name=\"y\">The other <see cref=\"int\"/> term.</param>", "        "));
+                AnalyzerAssert.Ast(expected, updated);
+            }
+
+            [Test]
+            public void WhenParamExistsInsertBefore()
+            {
+                var syntaxTree = CSharpSyntaxTree.ParseText(@"
+namespace RoslynSandbox
+{
+    public class Foo
+    {
+        /// <summary>Sum two numbers.</summary>
+        /// <param name=""y"">The other <see cref=""int""/> term.</param>
+        /// <returns>The sum.</returns>
+        public int Add(int x, int y) => x + y;
+    }
+}");
+                var expected = GetExpected(@"
+namespace RoslynSandbox
+{
+    public class Foo
+    {
+        /// <summary>Sum two numbers.</summary>
+        /// <param name=""x"">The first <see cref=""int""/> term.</param>
+        /// <param name=""y"">The other <see cref=""int""/> term.</param>
+        /// <returns>The sum.</returns>
+        public int Add(int x, int y) => x + y;
+    }
+}");
+                var method = syntaxTree.FindMethodDeclaration("Add");
+                Assert.AreEqual(true, method.TryGetDocumentationComment(out var comment));
+                var updated = comment.WithParamText("x", "The first <see cref=\"int\"/> term.");
+                AnalyzerAssert.Ast(expected, updated);
+
+                updated = comment.WithParam(Parse.XmlElementSyntax("<param name=\"x\">The first <see cref=\"int\"/> term.</param>", "        "));
                 AnalyzerAssert.Ast(expected, updated);
             }
 
