3af0630b5e45bae9463ef7c4fa8d38e415e5949c
JohanLarsson <johan_gavle1@hotmail.com>
2018-08-07 17:37:30 +0200

    StringHelper.IsFourParts

--- Gu.Roslyn.AnalyzerExtensions.Tests/StringHelperTests.cs
+++ Gu.Roslyn.AnalyzerExtensions.Tests/StringHelperTests.cs
@@ -16,15 +16,25 @@ namespace Gu.Roslyn.AnalyzerExtensions.Tests
         [TestCase("ab", "", "abc", false)]
         [TestCase("ab", null, "abc", false)]
         [TestCase("ab", "cd", "ab", false)]
-        public void IsParts(string text, string part1, string part2, bool expected)
+        public void IsTwoParts(string text, string part1, string part2, bool expected)
         {
             Assert.AreEqual(expected, text.IsParts(part1, part2));
         }
 
         [TestCase("<summary>Identifies the <see cref=\"Bar\"/> dependency property.</summary>", "<summary>Identifies the <see cref=\"", "Bar", "\"/> dependency property.</summary>", true)]
-        public void IsParts(string text, string part1, string part2, string part3, bool expected)
+        public void IsThreeParts(string text, string part1, string part2, string part3, bool expected)
         {
             Assert.AreEqual(expected, text.IsParts(part1, part2, part3));
+        }
+
+        [TestCase("abc", "a", "b", "c", null, true)]
+        [TestCase("abc", "a", "b", null, "c", true)]
+        [TestCase("abc", "a", null, "b", "c", true)]
+        [TestCase("abc", null, "a", "b", "c", true)]
+        [TestCase("abcd", "a", "b", "c", "d", true)]
+        public void IsFourParts(string text, string part1, string part2, string part3, string part4, bool expected)
+        {
+            Assert.AreEqual(expected, text.IsParts(part1, part2, part3, part4));
         }
     }
 }
--- Gu.Roslyn.AnalyzerExtensions/StringHelper.cs
+++ Gu.Roslyn.AnalyzerExtensions/StringHelper.cs
@@ -22,12 +22,12 @@ namespace Gu.Roslyn.AnalyzerExtensions
                 return start == null && end == null;
             }
 
-            if (start == null)
+            if (string.IsNullOrEmpty(start))
             {
                 return string.Equals(text, end, stringComparison);
             }
 
-            if (end == null)
+            if (string.IsNullOrEmpty(end))
             {
                 return string.Equals(text, start, stringComparison);
             }
@@ -57,17 +57,17 @@ namespace Gu.Roslyn.AnalyzerExtensions
                 return start == null && middle == null && end == null;
             }
 
-            if (start == null)
+            if (string.IsNullOrEmpty(start))
             {
                 return text.IsParts(middle, end, stringComparison);
             }
 
-            if (middle == null)
+            if (string.IsNullOrEmpty(middle))
             {
                 return text.IsParts(start, end, stringComparison);
             }
 
-            if (end == null)
+            if (string.IsNullOrEmpty(end))
             {
                 return text.IsParts(start, middle, stringComparison);
             }
@@ -80,6 +80,54 @@ namespace Gu.Roslyn.AnalyzerExtensions
             return text.StartsWith(start, stringComparison) &&
                    text.IndexOf(middle, start.Length, stringComparison) == start.Length &&
                    text.EndsWith(end, stringComparison);
+        }
+
+        /// <summary>
+        /// Check if <paramref name="text"/> is start + middle + end
+        /// </summary>
+        /// <param name="text">The text</param>
+        /// <param name="part1">Part 1</param>
+        /// <param name="part2">Part 2</param>
+        /// <param name="part3">Part 3</param>
+        /// <param name="part4">Part 4</param>
+        /// <param name="stringComparison">The <see cref="StringComparison"/></param>
+        /// <returns>True if <paramref name="text"/> is start + end</returns>
+        public static bool IsParts(this string text, string part1, string part2, string part3, string part4, StringComparison stringComparison = StringComparison.Ordinal)
+        {
+            if (text == null)
+            {
+                return part1 == null && part2 == null && part3 == null && part4 == null;
+            }
+
+            if (string.IsNullOrEmpty(part1))
+            {
+                return text.IsParts(part2, part3, part4, stringComparison);
+            }
+
+            if (string.IsNullOrEmpty(part2))
+            {
+                return text.IsParts(part1, part3, part4, stringComparison);
+            }
+
+            if (string.IsNullOrEmpty(part3))
+            {
+                return text.IsParts(part1, part2, part4, stringComparison);
+            }
+
+            if (string.IsNullOrEmpty(part4))
+            {
+                return text.IsParts(part1, part2, part3, stringComparison);
+            }
+
+            if (text.Length != part1.Length + part2.Length + part3.Length + part4.Length)
+            {
+                return false;
+            }
+
+            return text.StartsWith(part1, stringComparison) &&
+                   text.IndexOf(part2, part1.Length, stringComparison) == part1.Length &&
+                   text.IndexOf(part3, part1.Length + part2.Length, stringComparison) == part1.Length + part2.Length &&
+                   text.EndsWith(part4, stringComparison);
         }
     }
 }
