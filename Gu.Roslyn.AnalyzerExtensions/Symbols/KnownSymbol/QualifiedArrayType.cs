4254a37112d4343db31514d790163b17a7d58879
JohanLarsson <johan_gavle1@hotmail.com>
2018-10-12 11:35:37 +0200

    Docs.

--- Gu.Roslyn.AnalyzerExtensions/Symbols/KnownSymbol/QualifiedArrayType.cs
+++ Gu.Roslyn.AnalyzerExtensions/Symbols/KnownSymbol/QualifiedArrayType.cs
@@ -2,18 +2,29 @@ namespace Gu.Roslyn.AnalyzerExtensions
 {
     using Microsoft.CodeAnalysis;
 
+    /// <summary>
+    /// For comparison with <see cref="ITypeSymbol"/>.
+    /// </summary>
     public class QualifiedArrayType : QualifiedType
     {
         private static readonly NamespaceParts NamespaceParts = NamespaceParts.Create("System.Array");
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="QualifiedArrayType"/> class.
+        /// </summary>
+        /// <param name="elementType">The element type.</param>
         public QualifiedArrayType(QualifiedType elementType)
             : base(elementType.FullName + "[]", NamespaceParts, elementType.Type + "[]")
         {
             this.ElementType = elementType;
         }
 
+        /// <summary>
+        /// Gets the element type.
+        /// </summary>
         public QualifiedType ElementType { get; }
 
+        /// <inheritdoc />
         public override ITypeSymbol GetTypeSymbol(Compilation compilation) => compilation.CreateArrayTypeSymbol(this.ElementType.GetTypeSymbol(compilation));
     }
 }
