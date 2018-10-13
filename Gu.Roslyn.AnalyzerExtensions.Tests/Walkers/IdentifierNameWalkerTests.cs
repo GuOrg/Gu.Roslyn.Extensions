c258c10be240d299f2d12e4bcb911f724bc6d39c
JohanLarsson <johan_gavle1@hotmail.com>
2018-10-13 09:39:08 +0200

    Tests.

--- Gu.Roslyn.AnalyzerExtensions.Tests/Walkers/IdentifierNameWalkerTests.cs
+++ Gu.Roslyn.AnalyzerExtensions.Tests/Walkers/IdentifierNameWalkerTests.cs
@@ -1,6 +1,7 @@
 namespace Gu.Roslyn.AnalyzerExtensions.Tests.Walkers
 {
     using System.Linq;
+    using System.Threading;
     using Gu.Roslyn.Asserts;
     using Microsoft.CodeAnalysis.CSharp;
     using NUnit.Framework;
@@ -8,7 +9,7 @@ namespace Gu.Roslyn.AnalyzerExtensions.Tests.Walkers
     public class IdentifierNameWalkerTests
     {
         [Test]
-        public void Test()
+        public void TryFind()
         {
             var syntaxTree = CSharpSyntaxTree.ParseText(@"
 namespace RoslynSandbox
@@ -28,6 +29,66 @@ namespace RoslynSandbox
                 Assert.AreEqual(true, walker.TryFind("i", out var match));
                 Assert.AreEqual("i", match.Identifier.ValueText);
                 Assert.AreEqual(false, walker.TryFind("missing", out _));
+            }
+        }
+
+        [Test]
+        public void TryFindFirst()
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(@"
+namespace RoslynSandbox
+{
+    public class Foo
+    {
+        public Foo(int i)
+        {
+            i = 1;
+            i = 2;
+        }
+    }
+}");
+            var compilation = CSharpCompilation.Create("test", new[] { syntaxTree }, MetadataReferences.FromAttributes());
+            var semanticModel = compilation.GetSemanticModel(syntaxTree);
+            var symbol = semanticModel.GetDeclaredSymbolSafe(syntaxTree.FindParameter("int i"), CancellationToken.None);
+            var node = syntaxTree.FindTypeDeclaration("Foo");
+
+            Assert.AreEqual(true, IdentifierNameWalker.TryFindFirst(node, symbol, semanticModel, CancellationToken.None, out var match));
+            Assert.AreEqual(109, match.Identifier.SpanStart);
+
+            using (var walker = IdentifierNameWalker.Borrow(node))
+            {
+                Assert.AreEqual(true, walker.TryFindFirst(symbol, semanticModel, CancellationToken.None, out match));
+                Assert.AreEqual(109, match.Identifier.SpanStart);
+            }
+        }
+
+        [Test]
+        public void TryFindLast()
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(@"
+namespace RoslynSandbox
+{
+    public class Foo
+    {
+        public Foo(int i)
+        {
+            i = 1;
+            i = 2;
+        }
+    }
+}");
+            var compilation = CSharpCompilation.Create("test", new[] { syntaxTree }, MetadataReferences.FromAttributes());
+            var semanticModel = compilation.GetSemanticModel(syntaxTree);
+            var symbol = semanticModel.GetDeclaredSymbolSafe(syntaxTree.FindParameter("int i"), CancellationToken.None);
+            var node = syntaxTree.FindTypeDeclaration("Foo");
+
+            Assert.AreEqual(true, IdentifierNameWalker.TryFindLast(node, symbol, semanticModel, CancellationToken.None, out var match));
+            Assert.AreEqual(129, match.Identifier.SpanStart);
+
+            using (var walker = IdentifierNameWalker.Borrow(node))
+            {
+                Assert.AreEqual(true, walker.TryFindLast(symbol, semanticModel, CancellationToken.None, out match));
+                Assert.AreEqual(129, match.Identifier.SpanStart);
             }
         }
     }
