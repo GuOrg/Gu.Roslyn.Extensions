ae28ae6723307211293e5a40c6200a7cd53da3c2
Johan Larsson <johan_gavle1@hotmail.com>
2019-07-27 19:24:51 +0200

    UsingStatementWalker #55

---
+++ Gu.Roslyn.AnalyzerExtensions/Walkers/Simple/UsingStatementWalker.cs
@@ -0,0 +1,41 @@
+namespace Gu.Roslyn.AnalyzerExtensions
+{
+    using System.Collections.Generic;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+    /// <summary>
+    /// Find all <see cref="UsingStatementSyntax"/> in the scope.
+    /// </summary>
+    public sealed class UsingStatementWalker : PooledWalker<UsingStatementWalker>
+    {
+        private readonly List<UsingStatementSyntax> usingStatements = new List<UsingStatementSyntax>();
+
+        private UsingStatementWalker()
+        {
+        }
+
+        /// <summary>
+        /// A collection with the <see cref="UsingStatementSyntax"/> found when walking.
+        /// </summary>
+        public IReadOnlyList<UsingStatementSyntax> UsingStatements => this.usingStatements;
+
+        /// <summary>
+        /// Get a walker that has visited <paramref name="node"/>.
+        /// </summary>
+        /// <param name="node">The scope.</param>
+        /// <returns>A walker that has visited <paramref name="node"/>.</returns>
+        public static UsingStatementWalker Borrow(SyntaxNode node) => BorrowAndVisit(node, () => new UsingStatementWalker());
+
+        public override void VisitUsingStatement(UsingStatementSyntax node)
+        {
+            this.usingStatements.Add(node);
+            base.VisitUsingStatement(node);
+        }
+
+        protected override void Clear()
+        {
+            this.usingStatements.Clear();
+        }
+    }
+}
