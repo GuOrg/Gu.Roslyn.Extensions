13350ce0bd5c4111cc5a2edaad93fcca0a68291f
Johan Larsson <johan_gavle1@hotmail.com>
2019-07-27 18:34:01 +0200

    InvocationWalker #55

---
+++ Gu.Roslyn.AnalyzerExtensions/Walkers/Simple/InvocationWalker.cs
@@ -0,0 +1,38 @@
+namespace Gu.Roslyn.AnalyzerExtensions
+{
+    using System.Collections.Generic;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+    /// <summary>
+    /// Find all <see cref="InvocationExpressionSyntax"/> in the scope.
+    /// </summary>
+    public sealed class InvocationWalker : PooledWalker<InvocationWalker>
+    {
+        private readonly List<InvocationExpressionSyntax> invocations = new List<InvocationExpressionSyntax>();
+
+        private InvocationWalker()
+        {
+        }
+
+        /// <summary>
+        /// Get a walker that has visited <paramref name="node"/>.
+        /// </summary>
+        /// <param name="node">The scope.</param>
+        /// <returns>A walker that has visited <paramref name="node"/>.</returns>
+        public static InvocationWalker Borrow(SyntaxNode node) => BorrowAndVisit(node, () => new InvocationWalker());
+
+        /// <inheritdoc />
+        public override void VisitInvocationExpression(InvocationExpressionSyntax node)
+        {
+            this.invocations.Add(node);
+            base.VisitInvocationExpression(node);
+        }
+
+        /// <inheritdoc />
+        protected override void Clear()
+        {
+            this.invocations.Clear();
+        }
+    }
+}
