7020c32007a5b6744a97504fd630ef5b68c17f51
JohanLarsson <johan_gavle1@hotmail.com>
2019-07-30 15:02:31 +0200

    Move tests.

---
+++ Gu.Roslyn.CodeFixExtensions.Tests/CodeStyleTests/UsingDirectivesInsideNamespace.cs
@@ -0,0 +1,66 @@
+namespace Gu.Roslyn.CodeFixExtensions.Tests.CodeStyleTests
+{
+    using Gu.Roslyn.Asserts;
+    using Microsoft.CodeAnalysis.CSharp;
+    using NUnit.Framework;
+
+    public static class UsingDirectivesInsideNamespace
+    {
+        [Test]
+        public static void UsingDirectiveInsideNamespace()
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(@"
+namespace N
+{
+    using System;
+}");
+
+            var compilation = CSharpCompilation.Create("test", new[] { syntaxTree }, MetadataReferences.FromAttributes());
+            var semanticModel = compilation.GetSemanticModel(syntaxTree);
+            Assert.AreEqual(true, CodeStyle.UsingDirectivesInsideNamespace(semanticModel));
+        }
+
+        [Test]
+        public static void NoDirective()
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(@"
+namespace N
+{
+}");
+
+            var compilation = CSharpCompilation.Create("test", new[] { syntaxTree }, MetadataReferences.FromAttributes());
+            var semanticModel = compilation.GetSemanticModel(syntaxTree);
+            Assert.AreEqual(true, CodeStyle.UsingDirectivesInsideNamespace(semanticModel));
+        }
+
+        [Test]
+        public static void UsingDirectiveInsideAndOutsideNamespace()
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(@"
+using System;
+
+namespace N
+{
+    using System.Collections;
+}");
+
+            var compilation = CSharpCompilation.Create("test", new[] { syntaxTree }, MetadataReferences.FromAttributes());
+            var semanticModel = compilation.GetSemanticModel(syntaxTree);
+            Assert.AreEqual(true, CodeStyle.UsingDirectivesInsideNamespace(semanticModel));
+        }
+
+        [Test]
+        public static void UsingDirectiveOutsideNamespace()
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(@"
+using System;
+namespace N
+{
+}");
+
+            var compilation = CSharpCompilation.Create("test", new[] { syntaxTree }, MetadataReferences.FromAttributes());
+            var semanticModel = compilation.GetSemanticModel(syntaxTree);
+            Assert.AreEqual(false, CodeStyle.UsingDirectivesInsideNamespace(semanticModel));
+        }
+    }
+}
