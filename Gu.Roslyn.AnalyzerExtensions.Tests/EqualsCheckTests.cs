94e97017ad46f53e69bf43d84e1605b90f99d33f
JohanLarsson <johan_gavle1@hotmail.com>
2019-08-07 12:01:25 +0200

    HasEqualityOperator

--- Gu.Roslyn.AnalyzerExtensions.Tests/EqualsCheckTests.cs
+++ Gu.Roslyn.AnalyzerExtensions.Tests/EqualsCheckTests.cs
@@ -33,5 +33,61 @@ namespace N
             Assert.AreEqual("text", left.ToString());
             Assert.AreEqual("null", right.ToString());
         }
+
+        [TestCase("Equals(text, null)",                 true)]
+        [TestCase("object.Equals(text, null)",          true)]
+        [TestCase("Object.Equals(text, null)",          true)]
+        [TestCase("ReferenceEquals(text, null)",        false)]
+        [TestCase("object.ReferenceEquals(text, null)", false)]
+        [TestCase("Object.ReferenceEquals(text, null)", false)]
+        public void IsObjectEquals(string check, bool expected)
+        {
+            var code = @"
+namespace N
+{
+    using System;
+
+    class C
+    {
+        bool M(string text) => Equals(text, null);
+    }
+}".AssertReplace("Equals(text, null)", check);
+            var syntaxTree = CSharpSyntaxTree.ParseText(code);
+            var expression = syntaxTree.FindInvocation(check);
+            Assert.AreEqual(expected, EqualsCheck.IsObjectEquals(expression, out var left, out var right));
+            if (expected)
+            {
+                Assert.AreEqual("text", left.ToString());
+                Assert.AreEqual("null", right.ToString());
+            }
+        }
+
+        [TestCase("Equals(text, null)",                 false)]
+        [TestCase("object.Equals(text, null)",          false)]
+        [TestCase("Object.Equals(text, null)",          false)]
+        [TestCase("ReferenceEquals(text, null)",        true)]
+        [TestCase("object.ReferenceEquals(text, null)", true)]
+        [TestCase("Object.ReferenceEquals(text, null)", true)]
+        public void IsObjectReferenceEquals(string check, bool expected)
+        {
+            var code = @"
+namespace N
+{
+    using System;
+
+    class C
+    {
+        bool M(string text) => Equals(text, null);
+    }
+}".AssertReplace("Equals(text, null)", check);
+            var syntaxTree = CSharpSyntaxTree.ParseText(code);
+            var expression = syntaxTree.FindInvocation(check);
+            Assert.AreEqual(expected, EqualsCheck.IsObjectReferenceEquals(expression, out var left, out var right));
+            if (expected)
+            {
+                Assert.AreEqual("text", left.ToString());
+                Assert.AreEqual("null", right.ToString());
+            }
+        }
     }
 }
