3f9bdbf9cf33f91dd241963f488060b6ae70e7f4
Johan Larsson <johan_gavle1@hotmail.com>
2019-08-08 01:09:42 +0200

    Fix test.

--- Gu.Roslyn.CodeFixExtensions.Tests/CodeStyleTests/BackingFieldsAdjacent/DocumentEditor.cs
+++ Gu.Roslyn.CodeFixExtensions.Tests/CodeStyleTests/BackingFieldsAdjacent/DocumentEditor.cs
@@ -105,8 +105,7 @@ namespace N
         }
     }
 }");
-            Assert.AreEqual(CodeStyleResult.No, CodeStyle.BackingFieldsAdjacent(editor, out var newLineBetween));
-            Assert.AreEqual(true,                newLineBetween);
+            Assert.AreEqual(CodeStyleResult.No, CodeStyle.BackingFieldsAdjacent(editor, out _));
         }
 
         [Test]
