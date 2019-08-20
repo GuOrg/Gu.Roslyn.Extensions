fcb61d15fa7df9a19e9550293d5b11883331879b
JohanLarsson <johan_gavle1@hotmail.com>
2019-08-20 12:52:59 +0200

    SyntaxNode.IsInStaticContext()
    Close #74

--- Gu.Roslyn.AnalyzerExtensions.Tests/SyntaxTreeTests/SyntaxNodeExtTests.cs
+++ Gu.Roslyn.AnalyzerExtensions.Tests/SyntaxTreeTests/SyntaxNodeExtTests.cs
@@ -8,6 +8,57 @@ namespace Gu.Roslyn.AnalyzerExtensions.Tests.SyntaxTreeTests
 
     public static class SyntaxNodeExtTests
     {
+        [TestCase("\"abc\"", true)]
+        [TestCase("1", true)]
+        [TestCase("2", true)]
+        [TestCase("3", false)]
+        [TestCase("4", true)]
+        [TestCase("5", false)]
+        [TestCase("6", true)]
+        [TestCase("7", true)]
+        [TestCase("8", true)]
+        [TestCase("9", false)]
+        public static void IsInStaticContext(string expression, bool expected)
+        {
+            var code = @"
+namespace N
+{
+    using System;
+    using System.Linq.Expressions;
+
+    [Obsolete(""abc"")]
+    public class C
+    {
+        private readonly int f = 1;
+
+        public C()
+            :this(2)
+        {
+        }
+
+        public C(int _)
+        {
+            this.f = 3;
+        }
+
+        public static int P1 => 4;
+
+        public int P2 => 5;
+
+        public static int P3 { get; } = 6;
+
+        public int P4 { get; } = 7;
+
+        public static int M() => 8;
+
+        public int M() => 9;
+    }
+}";
+            var syntaxTree = CSharpSyntaxTree.ParseText(code);
+            var node = syntaxTree.Find<SyntaxNode>(expression);
+            Assert.AreEqual(expected, node.IsInStaticContext());
+        }
+
         [TestCase("1", true)]
         [TestCase("M", false)]
         public static void WhenReturningExpression(string text, bool expected)
