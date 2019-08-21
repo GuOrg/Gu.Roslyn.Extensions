a9bba690250e5701278f9405f03bb355d51cd6d6
JohanLarsson <johan_gavle1@hotmail.com>
2019-08-21 08:08:08 +0200

    Cleanup.

--- Gu.Roslyn.CodeFixExtensions/CodeStyle/CompilationStyleWalker.cs
+++ Gu.Roslyn.CodeFixExtensions/CodeStyle/CompilationStyleWalker.cs
@@ -2,7 +2,6 @@ namespace Gu.Roslyn.CodeFixExtensions
 {
     using System;
     using System.Collections.Generic;
-    using System.IO;
     using System.Threading;
     using System.Threading.Tasks;
     using Gu.Roslyn.AnalyzerExtensions;
