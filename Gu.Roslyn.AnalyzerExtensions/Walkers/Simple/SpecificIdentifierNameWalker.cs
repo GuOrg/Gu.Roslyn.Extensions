bb8454bf8eaac580146bee70fd4e57e424be1944
JohanLarsson <johan_gavle1@hotmail.com>
2019-11-05 12:21:38 +0100

    SpecificIdentifierNameWalker

---
+++ Gu.Roslyn.AnalyzerExtensions/Walkers/Simple/SpecificIdentifierNameWalker.cs
@@ -0,0 +1,57 @@
+namespace Gu.Roslyn.AnalyzerExtensions
+{
+    using System.Collections.Generic;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+    /// <summary>
+    /// Find all <see cref="IdentifierNameSyntax"/> matching the name in the scope.
+    /// </summary>
+    public sealed class SpecificIdentifierNameWalker : PooledWalker<SpecificIdentifierNameWalker>
+    {
+        private readonly List<IdentifierNameSyntax> identifierNames = new List<IdentifierNameSyntax>();
+        private string valueText = null!;
+
+        private SpecificIdentifierNameWalker()
+        {
+        }
+
+        /// <summary>
+        /// Gets a collection with the <see cref="IdentifierNameSyntax"/> found when walking.
+        /// </summary>
+        internal IReadOnlyList<IdentifierNameSyntax> IdentifierNames => this.identifierNames;
+
+        /// <summary>
+        /// Get a walker that has visited <paramref name="node"/>.
+        /// </summary>
+        /// <param name="node">The scope.</param>
+        /// <param name="valueText">The name to match <see cref="SyntaxToken.ValueText"/> with.</param>
+        /// <returns>A walker that has visited <paramref name="node"/>.</returns>
+        public static SpecificIdentifierNameWalker Borrow(SyntaxNode node, string valueText)
+        {
+            var walker = Borrow(() => new SpecificIdentifierNameWalker());
+            walker.valueText = valueText;
+            walker.Visit(node);
+            return walker;
+        }
+
+        /// <inheritdoc />
+        public override void VisitIdentifierName(IdentifierNameSyntax node)
+        {
+            if (node is { Identifier: { ValueText: { } candidate } } &&
+                candidate == this.valueText)
+            {
+                this.identifierNames.Add(node);
+            }
+
+            base.VisitIdentifierName(node);
+        }
+
+        /// <inheritdoc />
+        protected override void Clear()
+        {
+            this.valueText = null!;
+            this.identifierNames.Clear();
+        }
+    }
+}
