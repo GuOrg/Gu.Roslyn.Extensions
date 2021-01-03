58a437d6a9b7cd875278f1af2f45c84dd5471650
JohanLarsson <johan_gavle1@hotmail.com>
2021-01-03 13:05:36 +0100

    Simplify tests.

--- Gu.Roslyn.AnalyzerExtensions.Tests/SemanticModelExtTests/IsRepresentationPreservingConversion.cs
+++ Gu.Roslyn.AnalyzerExtensions.Tests/SemanticModelExtTests/IsRepresentationPreservingConversion.cs
@@ -8,23 +8,38 @@
 
     public static class IsRepresentationPreservingConversion
     {
-        [TestCase("Cast<int>(1)")]
-        [TestCase("Cast<int?>(1)")]
-        [TestCase("Cast<int?>(null)")]
-        [TestCase("Cast<double>(1.0)")]
-        [TestCase("Cast<double>(1d)")]
-        [TestCase("Cast<double>((double)1)")]
-        [TestCase("Cast<double>((System.Double)1)")]
-        [TestCase("Cast<System.IComparable>(1)")]
-        [TestCase("Cast<System.IComparable<int>>(1)")]
-        [TestCase("Cast<object>(1)")]
-        [TestCase("Cast<System.StringComparison>(System.StringComparison.CurrentCulture)")]
-        [TestCase("Cast<System.StringComparison>((object)System.StringComparison.CurrentCulture)")]
-        [TestCase("Cast<E>(E.M1)")]
-        [TestCase("Cast<object>(new object())")]
-        [TestCase("Cast<System.Collections.IEnumerable>(\"abc\")")]
-        [TestCase("Cast<System.Collections.IEnumerable?>(\"abc\")")]
-        public static void TrueWhen(string call)
+        [TestCase("Cast<int>(1)",                                                                  true)]
+        [TestCase("Cast<int>(null)",                                                               false)]
+        [TestCase("Cast<int>((object)null)",                                                       false)]
+        [TestCase("Cast<int>(1.0)",                                                                false)]
+        [TestCase("Cast<int>((object)1.0)",                                                        false)]
+        [TestCase("Cast<int>(new object())",                                                       false)]
+        [TestCase("Cast<int>((object)1)",                                                          true)]
+        [TestCase("Cast<int?>(1)",                                                                 true)]
+        [TestCase("Cast<int?>((object)1)",                                                         true)]
+        [TestCase("Cast<int?>(null)",                                                              true)]
+        [TestCase("Cast<int?>((object)null)",                                                      true)]
+        [TestCase("Cast<int?>(1.0)",                                                               false)]
+        [TestCase("Cast<int?>((object)1.0)",                                                       false)]
+        [TestCase("Cast<int>(n)",                                                                  true)]
+        [TestCase("Cast<int?>(n)",                                                                 true)]
+        [TestCase("Cast<int?>(nullableInt)",                                                       true)]
+        [TestCase("Cast<double>(1.0)",                                                             true)]
+        [TestCase("Cast<double>(1d)",                                                              true)]
+        [TestCase("Cast<double>(1)",                                                               false)]
+        [TestCase("Cast<double>((double)1)",                                                       true)]
+        [TestCase("Cast<double>((System.Double)1)",                                                true)]
+        [TestCase("Cast<double?>(1)",                                                              false)]
+        [TestCase("Cast<System.IComparable>(1)",                                                   true)]
+        [TestCase("Cast<System.IComparable<int>>(1)",                                              true)]
+        [TestCase("Cast<object>(1)",                                                               true)]
+        [TestCase("Cast<System.StringComparison>(System.StringComparison.CurrentCulture)",         true)]
+        [TestCase("Cast<System.StringComparison>((object)System.StringComparison.CurrentCulture)", true)]
+        [TestCase("Cast<E>(E.M1)",                                                                 true)]
+        [TestCase("Cast<object>(new object())",                                                    true)]
+        [TestCase("Cast<System.Collections.IEnumerable>(\"abc\")",                                 true)]
+        [TestCase("Cast<System.Collections.IEnumerable?>(\"abc\")",                                true)]
+        public static void When(string call, bool expected)
         {
             var e = @"
 namespace N
@@ -40,15 +55,14 @@ namespace N
 {
     public class C
     {
-        public C()
+        public C(int n, int? nullableInt)
         {
             var cast = Cast<int>(1);
         }
 
         private static T Cast<T>(object o) => (T) o;
     }
-}";
-            code = code.AssertReplace("Cast<int>(1)", call);
+}".AssertReplace("Cast<int>(1)", call);
             var syntaxTree = CSharpSyntaxTree.ParseText(code);
             var compilation = CSharpCompilation.Create(
                 "test",
@@ -59,73 +73,7 @@ namespace N
             var invocation = syntaxTree.FindInvocation(call);
             var method = semanticModel.GetSymbolSafe(invocation, CancellationToken.None);
             var expressionSyntax = invocation.ArgumentList.Arguments[0].Expression;
-            Assert.AreEqual(true, semanticModel.IsRepresentationPreservingConversion(expressionSyntax, method.ReturnType));
-        }
-
-        [TestCase("Cast<int?>(1)")]
-        [TestCase("Cast<int?>((object)1)")]
-        [TestCase("Cast<int?>(null)")]
-        [TestCase("Cast<int?>((object)null)")]
-        [TestCase("Cast<int?>(arg)")]
-        public static void TrueWhenNullable(string call)
-        {
-            var code = @"
-namespace N
-{
-    public class C
-    {
-        public C(int? arg)
-        {
-            var cast = Cast<int?>(arg);
-        }
-
-        private static T Cast<T>(object o) => (T) o;
-    }
-}";
-            code = code.AssertReplace("Cast<int?>(arg)", call);
-            var syntaxTree = CSharpSyntaxTree.ParseText(code);
-            var compilation = CSharpCompilation.Create(
-                "test",
-                new[] { syntaxTree },
-                MetadataReferences.FromAttributes(),
-                CodeFactory.DllCompilationOptions.WithNullableContextOptions(NullableContextOptions.Enable));
-            var semanticModel = compilation.GetSemanticModel(syntaxTree);
-            var invocation = syntaxTree.FindInvocation(call);
-            var method = semanticModel.GetSymbolSafe(invocation, CancellationToken.None);
-            var expressionSyntax = invocation.ArgumentList.Arguments[0].Expression;
-            Assert.AreEqual(true, semanticModel.IsRepresentationPreservingConversion(expressionSyntax, method.ReturnType));
-        }
-
-        [TestCase("Cast<int>(1.0)")]
-        [TestCase("Cast<int>(null)")]
-        [TestCase("Cast<int>((object)null)")]
-        [TestCase("Cast<int>((object)1.0)")]
-        [TestCase("Cast<int?>((object)1.0)")]
-        [TestCase("Cast<double>(1)")]
-        [TestCase("Cast<double?>(1)")]
-        public static void FalseWhen(string call)
-        {
-            var code = @"
-namespace N
-{
-    public class C
-    {
-        public C()
-        {
-            var cast = Cast<int>(1);
-        }
-
-        private static T Cast<T>(object o) => (T) o;
-    }
-}";
-            code = code.AssertReplace("Cast<int>(1)", call);
-            var syntaxTree = CSharpSyntaxTree.ParseText(code);
-            var compilation = CSharpCompilation.Create("test", new[] { syntaxTree }, MetadataReferences.FromAttributes());
-            var semanticModel = compilation.GetSemanticModel(syntaxTree);
-            var invocation = syntaxTree.FindInvocation(call);
-            var method = semanticModel.GetSymbolSafe(invocation, CancellationToken.None);
-            var expressionSyntax = invocation.ArgumentList.Arguments[0].Expression;
-            Assert.AreEqual(false, semanticModel.IsRepresentationPreservingConversion(expressionSyntax, method.ReturnType));
+            Assert.AreEqual(expected, semanticModel.IsRepresentationPreservingConversion(expressionSyntax, method.ReturnType));
         }
     }
 }
